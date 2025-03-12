554d4873dff07a61a74161c07dde6165ee31ee1a
Lucas Mazeau <luky974@gmail.com>
2025-03-12 23:49:02 +0100

    correction build

--- src/views/ProjetsView.vue
+++ src/views/ProjetsView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-[90vw]">
+  <div class="w-[70vw]">
     <h2 id="projets" class="text text-3xl lg:text-5xl w-max pl-4 pr-4 pb-8 text-center lg:text-left">projets</h2>
   <div class="pt-8 w-max ml-auto mr-auto">
     <grid-compo />
